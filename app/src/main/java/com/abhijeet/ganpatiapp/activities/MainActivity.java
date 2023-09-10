679b460ca35485d76cd8e816fb4ce6d37eb715df
Aryan <aryansood12@gmail.com>
2023-09-10 13:24:54 +0530

    Solved Google Sign In Bug

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/MainActivity.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/MainActivity.java
@@ -6,11 +6,103 @@ import android.os.Bundle;
 
 import com.abhijeet.ganpatiapp.R;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.abhijeet.ganpatiapp.adapters.MainPageAdapter;
+import com.abhijeet.ganpatiapp.modelclass.MainPageModelClass;
+import com.mig35.carousellayoutmanager.CarouselLayoutManager;
+import com.mig35.carousellayoutmanager.CarouselZoomPostLayoutListener;
+import com.mig35.carousellayoutmanager.CenterScrollListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    final CarouselLayoutManager layoutManager = new CarouselLayoutManager(CarouselLayoutManager.HORIZONTAL);
+    MainPageAdapter adapter;
+    List<MainPageModelClass> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        layoutManager.setPostLayoutListener(new CarouselZoomPostLayoutListener(2f));
+
+        initData();
+        initRecyclerView();
+
+        CardView profile = findViewById(R.id.cardView3);
+        profile.setOnClickListener(view -> profile());
+
+        CardView setting = findViewById(R.id.cardView);
+        setting.setOnClickListener(view -> setting());
+
+    }
+
+
+    public void profile(){
+        Intent intent = new Intent(MainActivity.this, Profile.class);
+        startActivity(intent);
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                vibrator.vibrate(50);
+            }
+        }
+    }
+
+    public void setting(){
+        Intent intent = new Intent(MainActivity.this, settings.class);
+        startActivity(intent);
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                vibrator.vibrate(50);
+            }
+        }
+    }
+
+    public void initRecyclerView() {
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addOnScrollListener(new CenterScrollListener());
+        layoutManager.setPostLayoutListener(new CarouselZoomPostLayoutListener());
+        adapter = new MainPageAdapter(list);
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void initData() {
+        list = new ArrayList<>();
+        list.add(new MainPageModelClass(Color.parseColor("#FFFFFF")));
+        list.add(new MainPageModelClass(Color.parseColor("#000000")));
+    }
+
+
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
     }
 }
