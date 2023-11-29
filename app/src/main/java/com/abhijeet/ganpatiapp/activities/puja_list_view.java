cbc3e4a29f0591f1680b35cdcae0a5843eedd22d
abhij <abhijeetpandeydhn@gmail.com>
2023-11-30 02:42:26 +0530

    all minor bug fixed.

---
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/puja_list_view.java
@@ -0,0 +1,16 @@
+package com.abhijeet.ganpatiapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.abhijeet.ganpatiapp.R;
+
+public class puja_list_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.puja_list_view);
+    }
+}
