14a89d42b5fa6cff43fa8cecb3bc2282360c1250
abhij <abhijeetpandeydhn@gmail.com>
2023-12-24 00:30:29 +0530

    added order system

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/puja_list_view.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/puja_list_view.java
@@ -6,10 +6,17 @@ import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.abhijeet.ganpatiapp.R;
 import com.abhijeet.ganpatiapp.adapters.Aarti_List_Adapter;
@@ -35,13 +42,13 @@ public class puja_list_view extends AppCompatActivity {
     RecyclerView puja_list_recycler_view;
     LinearLayoutManager layoutManager;
     PujaListViewAdapter adapter;
-    CardView backButton;
+    CardView backButton, sendBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.puja_list_view);
-
+        sendBtn = findViewById(R.id.order);
         list = new ArrayList<>();
         backButton = findViewById(R.id.backButton);
         title = findViewById(R.id.textView29);
@@ -77,6 +84,51 @@ public class puja_list_view extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String input = title.getText().toString();
+
+                String messagestr = "ORDER FOR PUJAN SAMAGRIH\n\n" + input; // Using the extracted input text
+
+                if (!input.isEmpty()) {
+                    if (iswhatsappInstalled()) {
+                        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=+919155182211&text=" + messagestr));
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(puja_list_view.this, "Whatsapp is not installed", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(puja_list_view.this, "All fields are required", Toast.LENGTH_LONG).show();
+                }
+
+                // Vibration code...
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vibrator != null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        vibrator.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+                    } else {
+                        vibrator.vibrate(50);
+                    }
+                }
+            }
+        });
+    }
+
+
+    private boolean iswhatsappInstalled() {
+        PackageManager packageManager = getPackageManager();
+        boolean whatsappInstalled;
+
+        try {
+            packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            whatsappInstalled = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            whatsappInstalled = false;
+        }
+        // Toast.makeText(this, String.valueOf(whatsappInstalled), Toast.LENGTH_SHORT).show();
+        return whatsappInstalled;
     }
 
     public void initRecyclerView() {
