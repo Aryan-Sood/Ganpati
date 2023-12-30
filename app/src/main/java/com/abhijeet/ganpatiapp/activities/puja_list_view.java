fea7535dc1b31a262108c72af80b645e7b581590
abhij <abhijeetpandeydhn@gmail.com>
2023-12-30 19:18:46 +0530

    updated fragment of card view

--- app/src/main/java/com/abhijeet/ganpatiapp/activities/puja_list_view.java
+++ app/src/main/java/com/abhijeet/ganpatiapp/activities/puja_list_view.java
@@ -1,11 +1,4 @@
 package com.abhijeet.ganpatiapp.activities;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -17,18 +10,19 @@ import android.os.Vibrator;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.abhijeet.ganpatiapp.R;
-import com.abhijeet.ganpatiapp.adapters.Aarti_List_Adapter;
 import com.abhijeet.ganpatiapp.adapters.PujaListViewAdapter;
-import com.abhijeet.ganpatiapp.modelclass.Aarti_List_Model_Class;
 import com.abhijeet.ganpatiapp.modelclass.Puja_List_View_model_Class;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,7 +32,6 @@ public class puja_list_view extends AppCompatActivity {
     DatabaseReference reference;
     TextView title;
     List<Puja_List_View_model_Class> list;
-
     RecyclerView puja_list_recycler_view;
     LinearLayoutManager layoutManager;
     PujaListViewAdapter adapter;
